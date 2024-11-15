10226c07924b3cea9d715d2ded530f38bc92cfb2
Vichers <lozex283@gmail.com>
2024-11-15 23:48:18 +0300

    Выполнил и протестировал тз 6-го проетка

---
+++ src/com/practicum/TaskManager/model/Node.java
@@ -0,0 +1,33 @@
+package com.practicum.TaskManager.model;
+
+public class Node {
+    Task task;
+    Node prev;
+    Node next;
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node (Node prev, Task task, Node next) {
+        this.task = task;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+}
--- src/com/practicum/TaskManager/service/InMemoryHistoryManager.java
+++ src/com/practicum/TaskManager/service/InMemoryHistoryManager.java
@@ -1,33 +1,93 @@
 package com.practicum.TaskManager.service;
 
 import com.practicum.TaskManager.model.Task;
+import com.practicum.TaskManager.model.Node;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Collections;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    final private Map<Integer, Task> history;
-    private int lastInHistory;
+    final private Map<Integer, Node> history;
+    private Node first;
+    private Node last;
 
     public InMemoryHistoryManager() {
         history = new HashMap<>();
-        lastInHistory = 10;
+        first = null;
+        last = null;
     }
 
     public void add(Task task) {
-        if (lastInHistory < 9) {
-            lastInHistory++;
-        } else {
-            lastInHistory = 0;
+        if (task == null) {
+            return;
+        }
+        if (history.containsKey(task.getId())) {
+            removeNode(history.get(task.getId()));
         }
 
-        history.put(lastInHistory, task);
+        linkLast(task);
+        history.put(task.getId(), last);
     }
 
     public List<Task> getHistory() {
-        return Collections.unmodifiableList(new ArrayList<>(history.values()));
+        boolean haveNextNode = true;
+        Node currentNode = last;
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        if (currentNode == null) {
+            return null;
+        }
+
+        while (haveNextNode) {
+            tasks.add(currentNode.getTask());
+            if (currentNode.getPrev() == null) {
+                haveNextNode = false;
+            } else {
+                currentNode = currentNode.getPrev();
+            }
+        }
+
+        return Collections.unmodifiableList(tasks);
+    }
+
+    public void remove(int id) {
+        if (history.get(id) == null) {
+            return;
+        }
+
+        removeNode(history.get(id));
+        history.remove(id);
+    }
+
+    private void linkLast(Task task) {
+        if (first == null) {
+            first = new Node (null, task, null);
+            last = first;
+        } else {
+            Node previous = last;
+            last = new Node(previous, task, null);
+            previous.setNext(last);
+        }
+    }
+
+    private void removeNode(Node node) {
+        Node prev = node.getPrev();
+        Node next = node.getNext();
+
+        if (prev == null && next == null) {
+            first = null;
+            last = null;
+            return;
+        } else if (prev == null) {
+            next.setPrev(null);
+            first = next;
+            return;
+        } else if (next == null) {
+            prev.setNext(null);
+            last = prev;
+            return;
+        }
+
+        prev.setNext(next);
+        next.setPrev(prev);
     }
 }
